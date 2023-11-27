023f30d5525c40ee4f9bcc9fea123354655bdea6
LanternCode <nazifltnts@gmail.com>
2023-11-27 23:32:23 +0100

    oct27

--- project1/user_data_nexp.sh
+++ project1/user_data_nexp.sh
@@ -22,8 +22,13 @@ ExecStart=/usr/local/bin/node_exporter
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# Nginx server installation
+#sudo apt-get update
+#sudo apt-get install nginx -y
+# Create a simple HTML file
+#echo '<html><head><title>Hello World! I am Nginx Server</title></head><body><h1>Hello, World NGINX_SERVER!</h1></body></html>' | sudo tee /var/www/html/index.html
 sudo systemctl enable node_exporter
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
 sudo systemctl status node_exporter
-
